d57d410b269f259a1e6b6e125a44a9e2c27756dd
hassangulzar1 <hassangulzar121@gmail.com>
2023-09-22 21:21:08 +0500

    date is left

--- src/Components/ModalInputs.jsx
+++ src/Components/ModalInputs.jsx
@@ -3,21 +3,40 @@ import { TextField } from "@mui/material";
 import useInput from "../hooks/use-input";
 
 const ModalInputs = () => {
+  //! Inputs States
   const {
     enteredValue: enteredId,
+    isValid: idIsValid,
     inputChangeHandler: idChangeHanlder,
     onBlurHandler: idBlurHandler,
     hasError: idInputIsValid,
     reset: resetId,
   } = useInput((value) => value.length == 4);
+  const {
+    enteredValue: enteredName,
+    isValid: nameIsValid,
+    inputChangeHandler: nameChangeHandler,
+    onBlurHandler: nameBlurHanlder,
+    hasError: nameInputIsValid,
+    reset: resetName,
+  } = useInput((value) => value.trim() !== "");
 
   const {
     enteredValue: enteredEmail,
+    isValid: emailIsValid,
     inputChangeHandler: emailChangeHandler,
     onBlurHandler: emailBlurHandler,
     hasError: emailInputIsValid,
     reset: emailReset,
   } = useInput((value) => value.includes("@gmail.com"));
+  const {
+    enteredValue: enteredSallary,
+    isValid: sallaryIsValid,
+    inputChangeHandler: sallaryChangeHandler,
+    onBlurHandler: sallaryBlurHandler,
+    hasError: sallaryInputIsValid,
+    reset: sallaryReset,
+  } = useInput((value) => value.trim() !== "");
 
   return (
     <div
@@ -37,10 +56,19 @@ const ModalInputs = () => {
         onChange={idChangeHanlder}
         onBlur={idBlurHandler}
         error={idInputIsValid}
-        maxLength={4}
         helperText={idInputIsValid ? "Please enter a valid Id (4 digits)" : ""}
       />
-      <TextField fullWidth sx={{ marginY: 1 }} type="text" label="Name" />
+      <TextField
+        fullWidth
+        sx={{ marginY: 1 }}
+        type="text"
+        label="User Name"
+        value={enteredName}
+        onChange={nameChangeHandler}
+        onBlur={nameBlurHanlder}
+        error={nameInputIsValid}
+        helperText={nameInputIsValid ? "Please enter a valid User Name" : ""}
+      />
       <TextField
         fullWidth
         sx={{ marginY: 1 }}
@@ -59,6 +87,11 @@ const ModalInputs = () => {
         sx={{ marginY: 1 }}
         type="number"
         label="sallary ($)"
+        value={enteredSallary}
+        onChange={sallaryChangeHandler}
+        onBlur={sallaryBlurHandler}
+        error={sallaryInputIsValid}
+        helperText={sallaryInputIsValid ? "Please enter a valid Sallary" : ""}
       />
       <TextField fullWidth sx={{ marginY: 1 }} type="text" label="Date" />
     </div>
